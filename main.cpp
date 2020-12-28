035ba05b42ec0b8b4ac156d30b4e319c30eaed75
Jwbrummett <73801390+Jwbrummett@users.noreply.github.com>
2020-12-28 00:27:08 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -16,9 +16,10 @@ struct stock{
 	float low_price; 
 	
 	// need the calculated values as well (how they did percentage wise etc. )
-	float daytrade, maxlong, maxshort; 
-}; 
+	float daytrade, maxlong, maxshort; }; 
 
+void sync (vector<stock>&, fstream&, string); 
+void add (fstream&, string); 
 
 int main (){
 	fstream f, w; 
@@ -33,71 +34,20 @@ int main (){
 		cout << dataline; 
 		getline(cin, command, '\n');
 
-		
+		switch (command)
 		if (command == "sync -l"){
-			f.open("stocks_dataset.txt"); 
-			 
-		
-			string tmp;
-			int linenum = 0; 
-			float hold; 
-			 
-			 
-			while (getline(f, tmp)){
-				
-				S.push_back(stock());
-				f >> S[linenum].ticker; 
-				
-				f >> S[linenum].overnight_percent; 
-				
-				f >> S[linenum].opening_price; 
-				
-				f >> S[linenum].closing_price; 
-				
-				f>> S[linenum].high_price; 
-				
-				f >> S[linenum].low_price; 
-								
-				S[linenum].daytrade = 1 - (S[linenum].opening_price / S[linenum].closing_price); 
-				
-				S[linenum].maxlong = 1 - (S[linenum].opening_price / S[linenum].high_price); 
-				
-				
-				linenum++;  
-	
-			}
+			sync (S, f, "stocks_dataset.txt");
+
+			cout << endl << "Data of overnight losers added" << endl << endl;
+			dataline = dataline + "/loserdata ";
 		}
 		else if (command == "sync -w"){
-			string tmp; 
-			int linenum = 0;
-			w.open ("data.txt");
-			
-			while (getline(w, tmp)){
-				L.push_back(stock());
-				w >> L[linenum].ticker;
-				
-				w >> L[linenum].overnight_percent; 
-				
-				w >> L[linenum].opening_price; 
-				
-				w >> L[linenum].closing_price; 
-				
-				w >> L[linenum].high_price; 
-				
-				w >> L[linenum].low_price; 
-								
-				L[linenum].daytrade = 1 - (L[linenum].opening_price / L[linenum].closing_price); 
-				
-				L[linenum].maxlong = 1 - (L[linenum].opening_price / L[linenum].high_price);
-				
-			} 
+			sync (L, w, "data.txt"); 
 			
 			cout << endl << "Data of overnight winners added" << endl << endl;
 			dataline = dataline + "/winnerdata ";  
 				 
-		}
-		// add the statistics to the dataset
-			
+		}			
 		else if (command == "add -l"){
 			f.open("stocks_dataset.txt", ios::app); 
 			
@@ -178,7 +128,7 @@ int main (){
 				
 				}
 			} while (tick != "NULL"); 	
-		}
+	
 			
 		}
 		else if (command == "q" || command == "Q"){
@@ -193,4 +143,38 @@ int main (){
 	
 	return 0; 
 }
+
+void sync (vector<stock>& S, fstream& f, string filename){
+	f.open(filename);  
+			 
+		
+			string tmp;
+			int i = 0; 			 
+			 
+			while (getline(f, tmp)){
+				
+				S.push_back(stock());
+				f >> S[i].ticker; 
+				
+				f >> S[i].overnight_percent; 
+				
+				f >> S[i].opening_price; 
+				
+				f >> S[i].closing_price; 
+				
+				f>> S[i].high_price; 
+				
+				f >> S[i].low_price; 
+								
+				S[i].daytrade = 1 - (S[i].opening_price / S[i].closing_price); 
+				
+				S[i].maxlong = 1 - (S[i].opening_price / S[i].high_price); 
+				
+				
+				i++;  
+				c
+	
+			}}
+void add (fstream&, string); 
+
 
